d999e78e6bc8df6ab901e424cec029b455f213d3
Roman Benz <kontakt@romanbenz.com>
2026-02-11 09:15:56 +0100

    Added Case-Insensitivity

---
+++ courses/coding-in-C/Lab_4/mySolution/Section3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void) {
+    int flag = 0;
+    char word[25];
+    printf("Enter a word: ");
+    scanf("%24s", word);
+
+    size_t elements_in_word = strlen(word);
+
+    for (size_t i = 0; i < elements_in_word / 2; i++)
+    {
+        if (tolower(word[i]) != tolower(word[elements_in_word - 1 - i]))
+        {
+           flag = 1;
+        }
+    }
+
+    if (flag == 1)
+    {
+        printf("Das Wort ist kein Palindrom");
+    }
+    else{
+        printf("Das Wort ist ein Palindrom");
+    }
+    
+    
+}
