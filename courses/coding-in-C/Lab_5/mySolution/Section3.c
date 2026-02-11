9aeec7e2844f8bcd4c3f03cb3ae69c30c8b454a1
Roman Benz <kontakt@romanbenz.com>
2026-02-11 10:58:17 +0100

    Added Solution to Lab5 Section3

---
+++ courses/coding-in-C/Lab_5/mySolution/Section3.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define timesteps 5
+#define array_size 10
+
+int array[array_size] = {0, 0, 0};
+
+int main(int argc, char **argv){
+
+    srand((unsigned)time(NULL));
+
+    //Partikel setzen
+    array[2] = 1;    
+    array[4] = 1;
+    array[6] = 1;
+
+    int *ptrarray = &array[0];
+
+    int temp[array_size];
+    for (int i = 0; i < array_size; i++)
+    {
+        temp[i] = 0;
+    }
+    
+    int *ptrtemp = &temp[0];
+
+    //Schleife für jeden Zeitschritt
+    for (int k = 0; k < timesteps; k++)
+    {
+        //Ausgabe
+        printf("Time %2d: ", k+1);
+        for (int k = 0; k < array_size; k++)
+        {
+           printf("%-2d", array[k]);
+        }
+        printf("\n");
+
+        //Schleife für jeden Array Element
+        for (int i = 0; i < array_size; i++)
+        {
+
+          if (array[i] == 1)        //Wenn Partikel da ist
+            {
+                
+                int r = rand() % 2;     
+
+                if(r == 0){         //Entscheiden mit random Zahl ob +1 oder -1 Position
+
+                    if ((i-1) >= 0)     //Check ob i im array liegt
+                    {
+
+                        if (temp[i-1] == 1)     //Wenn bei -1 bereits Partikel ist dann Kollision -> i-1 = 0
+                        {
+                            temp[i-1] = 0;
+                            printf("\nCollision on Index %d", i-1);
+                        }
+
+                        else{                   // Wenn bei -1 kein Partikel ist dann i-1 = 1
+                            temp[i-1] = 1;
+                        }
+                    }
+                    else{
+                        temp[i] = array[i];     //Wenn Partikel am Rand dann soll es am Rand bleiben
+                    }
+                    
+                }
+                else{
+
+                    if ( (i + 1) < array_size)  //Check ob i im array liegt
+                    {
+
+                        if (temp[i+1] == 1)     //Wenn bei +1 bereits Partikel ist dann Kollision -> i+1 = 0
+                        {
+                            temp[i+1] = 0;
+                            printf("\nCollision on Index %d", i+1);
+                        }
+
+                        else{                   // Wenn bei +1 kein Partikel ist dann i+1 = 1      
+                            temp[i+1] = 1;
+                        }
+
+                    }
+                    else{
+                        temp[i] = array[i];
+                    }
+                }
+            }  
+            //Ende innere Array-Schleife
+        } 
+        
+        //Array temp auf Array array kopieren
+        for (int j = 0; j < array_size; j++)
+        {
+            array[j] = temp[j];
+            
+        }
+        printf("\n\n");
+        //Temp Array nullen
+        for (int i = 0; i < array_size; i++)
+        {
+            temp[i] = 0;
+        }
+        
+    }   
+}
