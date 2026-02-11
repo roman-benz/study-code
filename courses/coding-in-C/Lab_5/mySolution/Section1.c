f8551910eabd7a86d40159eb3fb54207019abe1d
Roman Benz <kontakt@romanbenz.com>
2026-02-11 09:24:56 +0100

    Added Solution to Section1.c

---
+++ courses/coding-in-C/Lab_5/mySolution/Section1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+    int x = 10;
+    int *p = &x;
+    int **pp = &p;
+
+    // TODO: complete the output statements
+    printf("x = %d\n", x);
+    printf("x via p = %d\n", *p);
+    printf("x via pp = %d\n", **pp);
+
+    **pp = 42;
+    
+    printf("x = %d\n", x);
+    printf("x via p = %d\n", *p);
+    printf("x via pp = %d\n", **pp);
+
+    return 0;
+}
