3032b4877d4fdc296283552a136b80674b0df53a
Roman Benz <kontakt@romanbenz.com>
2026-02-27 14:27:41 +0100

    Added new features

--- courses/coding-in-C/Lab_8/mySolution/src/playlist.c
+++ courses/coding-in-C/Lab_8/mySolution/src/playlist.c
@@ -18,6 +18,30 @@ void song_anhaengen(Playlist *einePlaylist, Song *einSong){
             tempsong = tempsong->naechsterSong;
         }
         tempsong->naechsterSong = einSong;
+        einePlaylist->laenge++;
     }
     
+}
+
+Song* song1_entfernen(Playlist *einePlaylist){
+    if(einePlaylist->headsong == NULL){
+        return NULL;
+    }
+    else{
+        if (einePlaylist->headsong->naechsterSong == NULL)
+        {
+            Song *tempsong = einePlaylist->headsong;
+            einePlaylist->headsong = NULL;
+            einePlaylist->laenge--;
+            return tempsong;
+        }
+        else{
+            Song *tempsong = einePlaylist->headsong;
+            einePlaylist->headsong = einePlaylist->headsong->naechsterSong;
+            einePlaylist->laenge--;
+            
+        }
+        
+    }
+
 }
