474d89d7447b3876b5c8e41b93c67bde5bd058cd
Roman Benz <kontakt@romanbenz.com>
2026-02-27 14:22:45 +0100

    Added function song_anhaengen

--- courses/coding-in-C/Lab_8/mySolution/src/playlist.c
+++ courses/coding-in-C/Lab_8/mySolution/src/playlist.c
@@ -5,3 +5,19 @@ void init_playlist(Playlist *einePlaylist){
     einePlaylist->laenge = 0;
 }
 
+void song_anhaengen(Playlist *einePlaylist, Song *einSong){
+    if (einePlaylist->laenge == 0)
+    {
+        einePlaylist->headsong = einSong;
+        einePlaylist->laenge++;
+    }
+    else{
+        Song *tempsong = einePlaylist->headsong;
+        while (tempsong->naechsterSong != NULL)
+        {
+            tempsong = tempsong->naechsterSong;
+        }
+        tempsong->naechsterSong = einSong;
+    }
+    
+}
